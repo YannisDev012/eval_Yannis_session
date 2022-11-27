6cd19d1a7e842684764eed5fc680354859fad76f
YannisAdrar <yannisdev022@gmail.com>
2022-11-27 21:58:43 +0100

    codephp

---
+++ style/view/view_connexion.php
@@ -0,0 +1,16 @@
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>index>/title>
+</head>
+<body>
+   <h1>connexion : </h1>
+   <form action="" method="post">
+        <p>veuillez saisir votre identifiant : </p>
+        <input type="text" name="login">
+        <p>veuillez saisir votre mot de passe :</p>
+        <input type="text" name="mdp">
+        <input type="submit" value="connexion">
+    </form>
+</body>
+</html>
